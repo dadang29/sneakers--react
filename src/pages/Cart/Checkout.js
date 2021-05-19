925c1e955b3df6fdbf6341f6a81a0081bbcc878f
nisrina02 <niap1502@gmail.com>
2021-05-19 10:28:28 +0700

    Change Cart

--- src/pages/Cart/Checkout.js
+++ src/pages/Cart/Checkout.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Table, Radio, Divider, Typography } from 'antd';
+import {Link} from "react-router-dom";
+import {ShoppingCartOutlined} from "@ant-design/icons";
 import img1 from "../Image/90S VALASION SHOES.jpg";
 
 const data = [
@@ -7,7 +9,7 @@ const data = [
         key : 1,
         img : img1,
         name : "90S VALASION SHOES",
-        price : "Rp. 500.000",
+        price : 500000,
         qty : 1,
     },
 ];
@@ -15,7 +17,7 @@ const data = [
 const columns = [
     {
         title: "Gambar",
-        render: (data) => <img src={data.img} style={{ width: "130px" }} />
+        render: (data) => <img src={data.img} style={{ width: "100px" }} />
     },
     {
         title: "Nama Produk",
@@ -27,52 +29,55 @@ const columns = [
     },
     {
         title: "Subtotal",
+        // dataIndex: "price",
         render: (data) => <a>{data.price}</a>,
     },
 ];
 
-const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    getCheckboxProps: (record) => ({
-        disabled: record.name === 'Disabled User',
-        name: record.name
-    })
-};
+// const rowSelection = {
+//     onChange: (selectedRowKeys, selectedRows) => {
+//         console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+//     },
+//     getCheckboxProps: (record) => ({
+//         disabled: record.name === 'Disabled User',
+//         name: record.name
+//     })
+// };
 
 export const Checkout = () => {
     const {Text} = Typography;
-    const [selectionType, setSelectionType] = useState('checkbox');
+    // const [selectionType, setSelectionType] = useState('checkbox');
     return(
         <div style={{ marginTop: 30 }}>
             <Table
-                rowSelection={{
-                    type: selectionType,
-                    ...rowSelection,
-                }}
                 columns={columns}
                 dataSource={data}
                 size="middle"
+                bordered
 
-                // summary={pageData => {
-                //     let total = 0;
+                summary={pageData => {
+                    let total = 0;
 
-                //     pageData.forEach(({ price }) => {
-                //         total += price;
-                //     });
+                    pageData.forEach(({ price }) => {
+                        total += price;
+                    });
 
-                //     return(
-                //         <>
-                //             <Table.Summary.Row>
-                //                 <Table.Summary.Cell colspan={4}>Total</Table.Summary.Cell>
-                //                 <Table.Summary.Cell>
-                //                     <Text strong>{total}</Text>
-                //                 </Table.Summary.Cell>
-                //             </Table.Summary.Row>
-                //         </>
-                //     );
-                // }}
+                    return(
+                        <>
+                            <Table.Summary.Row>
+                                <Table.Summary.Cell colSpan={2}>
+                                    <Text strong style={{float:"right"}}>Total : </Text>
+                                </Table.Summary.Cell>
+                                <Table.Summary.Cell>
+                                    <Text strong>{total}</Text>
+                                </Table.Summary.Cell>
+                                <Table.Summary.Cell>
+                                    <Link className="btn btn-success"><ShoppingCartOutlined style={{ fontSize: "20px" }}/> Checkout </Link>
+                                </Table.Summary.Cell>
+                            </Table.Summary.Row>
+                        </>
+                    );
+                }}
             />
         </div>
     );
